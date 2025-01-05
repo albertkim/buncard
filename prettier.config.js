b71acb6fa16ee84265c75b60aa3c6391069cc59f
Albert Kim <albert275@gmail.com>
2025-01-05 10:12:54 -0800

    Boilerplate vite react typescript tailwind setup

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ["prettier-plugin-organize-imports"],
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: false,
+  printWidth: 120,
+  singleQuote: false,
+  endOfLine: "auto"
+}
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import "./App.css"
+
+export function App() {
+  return (
+    <div className="flex">
+      <h1 className="text-xl">Hello Tailwind + Vite!</h1>
+    </div>
+  )
+}
