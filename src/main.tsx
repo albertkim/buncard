35cca5883f371a63a29c5e3ebda695a910af5e60
Albert Kim <albert275@gmail.com>
2025-01-05 16:18:04 -0800

    Remove service worker registration in main, supposedly vite pwa plugin handles that, also update deprecated apple app meta tag

--- src/main.tsx
+++ src/main.tsx
@@ -8,10 +8,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <App />
   </React.StrictMode>
 )
-
-// Add this PWA registration code
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js")
-  })
-}
